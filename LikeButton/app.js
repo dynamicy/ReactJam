1b61544efdc435fa9a7f30696de77aad98ff7c90
dynamicy <fermat.little.theorem@gmail.com>
2016-06-27 00:43:22 +0800

    Add LikeButton.

---
+++ LikeButton/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LikeButton from './components/LikeButton.jsx';
+
+ReactDOM.render(
+    <LikeButton />, 
+document.getElementById('app'));
